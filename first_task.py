67b6922555f4d7bca5f9aa2c6ffb085251332eee
Aidan Tatysheva <tatyshevaaidan@gmail.com>
2021-10-04 10:41:47 +0300

    some files changed

--- first_task.py
+++ first_task.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 if __name__ == '__main__':
     school = {
         '1a': 31,
--- second_task.py
+++ second_task.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 if __name__ == '__main__':
     student = {
         1: 'anton',
